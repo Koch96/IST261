3ac257772b48387d7a24987c0d623a1787e99425
mullanator216 <59973723+mullanator216@users.noreply.github.com>
2020-03-05 13:10:25 -0500

    Add files via upload

---
+++ IST261/src/IST261/Test.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package IST261;
+
+/**
+ *
+ * @author jmm7925
+ * @version 0.1
+ * @since Mar 5, 2020 1:08:19 PM
+ * 
+ */
+
+/**           MODIFICATION LOG
+
+    Mar 5, 2020 Initial File Creation at 1:08:19 PM
+
+*/
+
+
+public class Test 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello");
+    }
+
+}// class Test
